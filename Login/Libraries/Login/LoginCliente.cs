de4c3f1e93080e761b1d30eb3407cde927afe2d4
CloudCodex <cloudcodexx@gmail.com>
2025-11-14 08:12:15 -0300

    Criando o LoginCliente e LoginColaborador

---
+++ Login/Libraries/Login/LoginCliente.cs
@@ -0,0 +1,35 @@
+﻿using Login.Models;
+using Newtonsoft.Json;
+using Login.Libraries.Sessao;
+
+namespace Login.Libraries.Login
+{
+    public class LoginCliente
+    {
+        private string Key = "Login.Cliente";
+        private Sessao.Sessao _sessao;
+
+        public LoginCliente(Sessao.Sessao sessao)
+        {
+            _sessao = sessao;
+        }
+
+        public void Login(Cliente cliente)
+        {
+            string Cliente.JSONString = JsonConvert.SerializeObject(cliente);
+            _sessao Cadastrar(Key, clienteJSONString);
+        }
+
+        public Cliente getCliente()
+        {
+            if (_sessao.Existe(Key))
+            {
+                string clienteJSONString = _sessao.Consultar(Key);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Login/Libraries/Login/LoginColaborador.cs
@@ -0,0 +1,36 @@
+﻿using Login.Models;
+using Newtonsoft.Json;
+using Login.Libraries.Sessao;
+
+namespace Login.Libraries.Login
+{
+    public class LoginColaborador
+    {
+        private string Key = "Login.Cliente";
+        private Sessao.Sessao _sessao;
+
+        public LoginColaborador(Sessao.Sessao sessao)
+        {
+            _sessao = sessao;
+        }
+
+        public void Login(Colaborador colaborador)
+        {
+            string ColaboradorJSONString = JsonConvert.SerializeObject(colaborador);
+            _sessao.Cadastrar(Key, ColaboradorJSONString);
+        }
+
+        public Colaborador getColaborador()
+        {
+            if (_sessao.Existe(Key))
+            {
+                string colaboradorJSONString = _sessao.Consultar(Key);
+                return JsonConvert.DeserializeObject<Colaborador>(colaboradorJSONString);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
